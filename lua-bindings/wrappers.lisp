4538581dc2a8f05221641fc6a255e3a47f1bf8cc
Johannes Maier <johannes.maier@mailbox.org>
2024-08-15 23:37:17 +0200

    lua: use macros for thin wrappers

--- lua-bindings/wrappers.lisp
+++ lua-bindings/wrappers.lisp
@@ -1,32 +1,29 @@
 (in-package :lua)
 
-(defun call (ls nargs nresults)
-  (callk ls nargs nresults 0 (null-pointer)))
+(defmacro call (ls nargs nresults)
+  `(callk ,ls ,nargs ,nresults 0 (null-pointer)))
 
-(defun pcall (ls nargs nresults errfunc)
-  (pcallk ls nargs nresults errfunc 0 (null-pointer)))
+(defmacro pcall (ls nargs nresults errfunc)
+  `(pcallk ,ls ,nargs ,nresults ,errfunc 0 (null-pointer)))
 
-(defun pushcfunction (ls f)
-  (pushcclosure ls f 0))
+(defmacro pushcfunction (ls f)
+  `(pushcclosure ,ls ,f 0))
 
-(defun loadfile (ls filename)
-  (loadfilex ls filename (null-pointer)))
+(defmacro loadfile (ls filename)
+  `(loadfilex ,ls ,filename (null-pointer)))
 
 (defun dofile (ls filename)
   (let ((res (loadfile ls filename)))
     (when (= 0 res)
       (pcall ls 0 -1 0))))
 
-(defun pushfunction (ls f)
+(defmacro pushfunction (ls f)
   "Push a Lisp function/closure f to Lua. The function
 must take the Lua state as argument and then evaluate to the number of values
 that have been pushed onto the Lua stack."
-  (cffi:defcallback cb :int ((l lua-state)) (funcall f l))
-  (pushcfunction ls (cffi:callback cb)))
-
-(defun register (ls name f)
-  (pushfunction ls f)
-  (setglobal ls name))
+  `(progn
+     (cffi:defcallback cb :int ((l lua-state)) (funcall ,f l))
+     (pushcfunction ,ls (cffi:callback cb))))
 
 (defmacro newtable (ls)
   `(createtable ,ls 0 0))
